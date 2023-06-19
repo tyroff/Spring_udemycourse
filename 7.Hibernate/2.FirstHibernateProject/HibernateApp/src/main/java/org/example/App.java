9af07e3aaf70ee4a138833973f1869b1bfc33d46
Kirill Vinokorov <kirill.vinokurov@itvert.com>
2023-06-19 15:44:43 +0300

    First Hibernate project

---
+++ 7.Hibernate/2.FirstHibernateProject/HibernateApp/src/main/java/org/example/App.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        // Configuration читает файл hibernate.properties (лишь с таким названием по умолчанию)
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            Person person = session.get(Person.class, 1);
+
+            System.out.println(person.getName());
+            System.out.println(person.getAge());
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
