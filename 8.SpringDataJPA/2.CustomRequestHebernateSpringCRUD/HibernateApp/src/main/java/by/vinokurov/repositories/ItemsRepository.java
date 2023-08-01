31c134b0f3e460427587fce30ba19afaefab6772
Kirill Vinokurov <tyroff@mail.ru>
2023-08-02 00:20:18 +0300

    custom request in Hibernate Spring

---
+++ 8.SpringDataJPA/2.CustomRequestHebernateSpringCRUD/HibernateApp/src/main/java/by/vinokurov/repositories/ItemsRepository.java
@@ -0,0 +1,15 @@
+package by.vinokurov.repositories;
+
+import by.vinokurov.models.Item;
+import by.vinokurov.models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ItemsRepository extends JpaRepository<Item, Integer> {
+    List<Item> findByItemName(String itemName);
+
+    List<Item> findByOwner(Person owner);
+}
