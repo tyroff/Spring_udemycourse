c49cb3924ee8dd315b244bbf68889ff7693cd7d2
Kirill Vinokurov <tyroff@mail.ru>
2023-11-09 23:48:13 +0300

    new: Create a custom login page and set it up to work

--- 12.SpringSecurity/81.CustomLoginPage/FirstSecurityApp/src/main/java/by/vinokurov/springcourse/SecurityApp/config/SecurityConfig.java
+++ 12.SpringSecurity/81.CustomLoginPage/FirstSecurityApp/src/main/java/by/vinokurov/springcourse/SecurityApp/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import by.vinokurov.springcourse.SecurityApp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,7 +20,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.personDetailsService = personDetailsService;
     }
 
+    protected void configure(HttpSecurity http) throws Exception {
+        //конфигурируем сам Spring Security
+        //конфигурируем авторизацию
+        http.csrf().disable() //Отключаем защиту от межсайтовой подделки запросов
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/lagin/error");
+    }
+
     //настраивает аутентификацию
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
     }
---
+++ 12.SpringSecurity/81.CustomLoginPage/FirstSecurityApp/src/main/java/by/vinokurov/springcourse/SecurityApp/controllers/AuthController.java
@@ -0,0 +1,16 @@
+package by.vinokurov.springcourse.SecurityApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "auth/login";
+    }
+
+}
