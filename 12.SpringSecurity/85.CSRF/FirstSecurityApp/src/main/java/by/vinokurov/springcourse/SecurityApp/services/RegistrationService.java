fb1b42da06775b9ab6e7adbd5b13121444edfad9
Kirill Vinokurov <tyroff@mail.ru>
2023-11-17 22:51:10 +0300

    start: CSRF

---
+++ 12.SpringSecurity/85.CSRF/FirstSecurityApp/src/main/java/by/vinokurov/springcourse/SecurityApp/services/RegistrationService.java
@@ -0,0 +1,26 @@
+package by.vinokurov.springcourse.SecurityApp.services;
+
+import by.vinokurov.springcourse.SecurityApp.models.Person;
+import by.vinokurov.springcourse.SecurityApp.repositories.PeopleRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class RegistrationService {
+
+    private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public RegistrationService(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
+        this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public void registration(Person person) {
+        String encodedPassword = passwordEncoder.encode(person.getPassword());
+        person.setPassword(encodedPassword);
+        peopleRepository.save(person);
+    }
+}
