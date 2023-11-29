d7f93d627ccc6162d0aebc1a7d21bbc2fcb96b3f
Kirill Vinokurov <tyroff@mail.ru>
2023-11-29 12:55:54 +0300

    start: Create with the @ExceptionHandler

---
+++ 13.SpringREST/92.AnnotationExceptionHandler/FirstRestApp/src/main/java/by/vinokurov/FirstRestApp/models/Person.java
@@ -0,0 +1,70 @@
+package by.vinokurov.FirstRestApp.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name ="person")
+public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "email")
+    private String email;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
